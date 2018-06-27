6949403f96be4559e01021c5a19dc65cd08ebbeb
Ardianpurnama <31305690+Ardianpurnama@users.noreply.github.com>
2018-06-28 04:37:13 +0700

    Add files via upload

---
+++ TokenS.py
@@ -0,0 +1,14 @@
+#def Login(resp, auth):
+
+#bot = RidenBOT(resp, auth)
+
+#Riden = threading.Thread(target=login, args=('Riden','YOUR TOKEN')).start()
+
+#Riden1 = threading.Thread(target=login, args=('Riden1','YOUR TOKEN')).start()
+
+#Riden2 = threading.Thread(target=login, args=('Riden2','YOUR TOKEN')).start()
+
+#Riden2 = threading.Thread(target=login, args=('Riden3','YOUR TOKEN')).start()
+
+
+#print("LOGIN SUCCESS RFU")
