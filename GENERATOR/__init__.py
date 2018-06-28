79ede61102df4ec187a81b3a839645e120b320a1
Ardianpurnama <31305690+Ardianpurnama@users.noreply.github.com>
2018-06-28 16:29:58 +0700

    Add files via upload

---
+++ GENERATOR/__init__.py
@@ -0,0 +1,6 @@
+from .client import RIDEN
+from .channel import RIDENChannel
+from .oepoll import RIDENPoll
+from akad.ttypes import OpType
+
+__all__ = ['RIDEN', 'RIDENChannel', 'RIDENPoll', 'OpType']
